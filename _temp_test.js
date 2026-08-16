ea18567e059de3e9cd73d5b13f6e544d3298e700
Bibek Mahata <mahatabibek49@gmail.com>
2026-08-16 19:33:54 +0530

    Master Feature: Integrate Live Bengal Intelligence Hub with 4 Real-Time APIs (Open-Meteo Air Quality AQI Smog Radar, Solar & Golden-Hour Photography Tracker, Open Exchange Currency Converter, and District Fuel Engine)

---
+++ _temp_test.js
@@ -0,0 +1,19 @@
+
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Bengal Explorers",
+        "alternateName": ["Bibek Mahata", "Bengal Explorers by Bibek Mahata", "Bibek Mahata Portal", "BengalExplorers"],
+        "url": "https://bibekmahata-dev.github.io/",
+        "author": {
+            "@type": "Person",
+            "name": "Bibek Mahata",
+            "url": "https://bibekmahata-dev.github.io/about.html"
+        },
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://bibekmahata-dev.github.io/?q={search_term_string}",
+            "query-input": "required name=search_term_string"
+        }
+    }
+    
