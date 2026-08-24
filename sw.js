3c3cf64c3bf2a017d4296067d488d525a42b395d
Bibek Mahata <mahatabibek49@gmail.com>
2026-08-24 19:25:21 +0530

    chore(sw): bump cache to bengal-explorers-v15 for automated obsolete cache purge

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
    Version: 1.0.0
    ============================================================================== */
 
-const CACHE_NAME = 'bengal-explorers-v14';
-const DATA_CACHE_NAME = 'bengal-data-cache-v14';
+const CACHE_NAME = 'bengal-explorers-v15';
+const DATA_CACHE_NAME = 'bengal-data-cache-v15';
 
 // Critical Core Assets for Offline App Shell
 const APP_SHELL = [
