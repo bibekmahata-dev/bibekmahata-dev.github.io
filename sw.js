d030a574b59b42e2309a82e9b65d631eb623cb92
Bibek Mahata <mahatabibek49@gmail.com>
2026-08-29 07:58:58 +0530

    feat(global): launch 1-click multi-language quick ribbon, Fastly edge CDN telemetry & universal schema suite (SW v35)

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
    Version: 1.0.0
    ============================================================================== */
 
-const CACHE_NAME = 'bengal-explorers-v34';
-const DATA_CACHE_NAME = 'bengal-data-cache-v34';
+const CACHE_NAME = 'bengal-explorers-v35';
+const DATA_CACHE_NAME = 'bengal-data-cache-v35';
 
 // Critical Core Assets for Offline App Shell
 const APP_SHELL = [
