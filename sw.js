840f2d977e48ad12b4c60a2802c0e1f2062fc354
Bibek Mahata <mahatabibek49@gmail.com>
2026-08-29 22:06:44 +0530

    feat(data): enrich Point-to-Point Trains (Vande Bharat, Tamralipta, Ispat) & WBTDCL/WBFDC Govt Lodges (Darjeeling, Jhargram, Sajnekhali, Shantiniketan, Lava) (SW v56)

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
    Version: 1.0.0
    ============================================================================== */
 
-const CACHE_NAME = 'bengal-explorers-v55';
-const DATA_CACHE_NAME = 'bengal-data-cache-v55';
+const CACHE_NAME = 'bengal-explorers-v56';
+const DATA_CACHE_NAME = 'bengal-data-cache-v56';
 
 // Critical Core Assets for Offline App Shell
 const APP_SHELL = [
