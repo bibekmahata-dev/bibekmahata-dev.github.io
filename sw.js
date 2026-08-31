cdb80e432e74f9c173e416455bc5830e8f55c93b
Bibek Mahata <mahatabibek49@gmail.com>
2026-08-31 18:52:59 +0530

    feat(blog): add 3 new master visual field guides (Tiger Hill Sunrise, Bishnupur Terracotta Walking Trail, North Kolkata Heritage Food Trail) - 11 total guides (SW v69)

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
    Version: 1.0.0
    ============================================================================== */
 
-const CACHE_NAME = 'bengal-explorers-v68';
-const DATA_CACHE_NAME = 'bengal-data-cache-v68';
+const CACHE_NAME = 'bengal-explorers-v69';
+const DATA_CACHE_NAME = 'bengal-data-cache-v69';
 
 // Critical Core Assets for Offline App Shell
 const APP_SHELL = [
