c9e23499586f7fad680d1b9d5a8d5d277ea6df44
Bibek Mahata <mahatabibek49@gmail.com>
2026-09-18 12:10:46 +0530

    feat(seo): optimize keywords for Bengal Travel & Bengal Explore Website, prioritize Bibek Mahata search ranking

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
    Version: 1.0.0
    ============================================================================== */
 
-const CACHE_NAME = 'bengal-explorers-v114';
-const DATA_CACHE_NAME = 'bengal-data-cache-v114';
+const CACHE_NAME = 'bengal-explorers-v115';
+const DATA_CACHE_NAME = 'bengal-data-cache-v115';
 
 // Critical Core Assets for Offline App Shell
 const APP_SHELL = [
